cfa7d3d82d98c89bdce3be52304b9d66bf3682be
Daniele Prada <daniele.prada85@gmail.com>
2023-01-23 17:11:21 +0100

    Simplified travis and tox configuration files

--- setup.py
+++ setup.py
@@ -17,16 +17,15 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Daniele Prada",
     author_email='prada@imati.cnr.it',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="A Python package for solving partial differential equations by the Virtual Element Method.",
     install_requires=requirements,
